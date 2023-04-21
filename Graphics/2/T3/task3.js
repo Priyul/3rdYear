97afdcedc7d6af144690314bae97f75039fea1b8
Priyul <Priyul20@gmail.com>
2023-04-21 15:04:31 +0200

    🎉 task3 done

--- Graphics/2/T3/task3.js
+++ Graphics/2/T3/task3.js
@@ -94,13 +94,20 @@ const roof3Vertices = [ //right
     -0.15, 0.0, 0.01
   ];
 
+  const baseVertices = [
+    -0.5, -0.5, 0.0,
+    0.5, -0.5, 0.0,
+    0.5, -0.5, -1.0,
+    -0.5, -0.5, -1.0
+  ];
+
 
   
-  const baseColor = [1.0, 0.0, 0.0, 1.0]; // Red
-  const roof1Color = [0.0, 0.0, 1.0, 1.0]; // front
-  const roof2Color = [0.0, 1.0, 0.0, 1.0]; // left
-  const roof3Color = [0.0, 1.0, 0.0, 1.0]; // right
-  const roof4Color = [0.0, 0.0, 1.0, 1.0]; // back
+  const baseColor = [0.0, 0.0, 1.0, 1.0]; // BLUE
+  const roof1Color = [0.0, 0.0, 1.0, 1.0]; // front - BLUE
+  const roof2Color = [0.0, 1.0, 0.0, 1.0]; // left - green
+  const roof3Color = [0.0, 1.0, 0.0, 1.0]; // right - green
+  const roof4Color = [0.0, 0.0, 1.0, 1.0]; // back - blue
   const doorColor = [0.0, 1.0, 0.0, 1.0]; // Green
 
   const vertexShaderSource = `
@@ -202,6 +209,11 @@ const rightWindowBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, rightWindowBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(rightWindowVertices), gl.STATIC_DRAW);
 
+const baseBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, baseBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(baseVertices), gl.STATIC_DRAW);
+
+
 // const leftWallVAO = createVAO(gl, leftWallBuffer, program);
 // const rightWallVAO = createVAO(gl, rightWallBuffer, program);
 // const frontWallVAO = createVAO(gl, frontWallBuffer, program);
@@ -228,48 +240,48 @@ function drawPart(gl, buffer, vertices, color) {
 
 
 function drawScene(time) {
-  // gl.clearColor(0.8, 0.9, 1.0, 1.0); // Set a background color (sky blue)
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+  
+    const aspect = canvas.width / canvas.height;
+    const projectionMatrix = perspective(45, aspect, 1, 100);
+    const viewMatrix = lookAt(vec3(0, 0, 5), vec3(0, 0, 0), vec3(0, 1, 0));
+    const modelMatrix = mat4();
+  
+    const rotationAxis = document.querySelector('input[name="rotation"]:checked').value;
+  
+    let rotationMatrix;
+    if (rotationAxis === "x") {
+      rotationMatrix = rotateX(time / 5);
+    } else if (rotationAxis === "y") {
+      rotationMatrix = rotateY(time / 5);
+    } else {
+      rotationMatrix = rotateZ(time / 5);
+    }
+  
+    const finalModelMatrix = mult(modelMatrix, rotationMatrix);
+    const mvpMatrix = mult(projectionMatrix, mult(viewMatrix, finalModelMatrix));
+  
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, 'u_mvpMatrix'), false, flatten(mvpMatrix));
+  
+    drawPart(gl, frontWallBuffer, frontWallVertices, frontWallColor); // Front wall
+    drawPart(gl, leftWallBuffer, leftWallVertices, leftWallColor); // Left wall
+    drawPart(gl, rightWallBuffer, rightWallVertices, rightWallColor); // Right wall
+    drawPart(gl, backWallBuffer, backWallVertices, backWallColor); // Back wall
+  
+    drawPart(gl, roof1Buffer, roof1Vertices, roof1Color);
+    drawPart(gl, roof2Buffer, roof2Vertices, roof2Color);
+    drawPart(gl, roof3Buffer, roof3Vertices, roof3Color);
+    drawPart(gl, roof4Buffer, roof4Vertices, roof4Color);
+  
+    drawPart(gl, doorBuffer, doorVertices, doorColor);
+    drawPart(gl, leftWindowBuffer, leftWindowVertices, leftWindowColor);
+    drawPart(gl, rightWindowBuffer, rightWindowVertices, rightWindowColor);
+  
+    drawPart(gl, baseBuffer, baseVertices, baseColor);
 
-  const aspect = canvas.width / canvas.height;
-  const projectionMatrix = perspective(45, aspect, 1, 100);
-  const viewMatrix = lookAt(vec3(0, 0, 5), vec3(0, 0, 0), vec3(0, 1, 0));
-  const modelMatrix = mat4();
-
-  // if (time >= 360) {
-  //     time = 0;
-  //   }
-  const rotationMatrix = rotateY(time/5); 
-  // const rotationMatrix = rotateY(270); 
-
-
-  console.log(time);
-
-  const finalModelMatrix = mult(modelMatrix, rotationMatrix);
-  const mvpMatrix = mult(projectionMatrix, mult(viewMatrix, finalModelMatrix));
-
-  gl.uniformMatrix4fv(gl.getUniformLocation(program, 'u_mvpMatrix'), false, flatten(mvpMatrix));
-
-  drawPart(gl, frontWallBuffer, frontWallVertices, frontWallColor); // Front wall
-  drawPart(gl, leftWallBuffer, leftWallVertices, leftWallColor); // Left wall
-  drawPart(gl, rightWallBuffer, rightWallVertices, rightWallColor); // Right wall
-  drawPart(gl, backWallBuffer, backWallVertices, backWallColor); // Back wall
-
-
-  drawPart(gl, roof1Buffer, roof1Vertices, roof1Color);
-  drawPart(gl, roof2Buffer, roof2Vertices, roof2Color);
-  drawPart(gl, roof3Buffer, roof3Vertices, roof3Color);
-  drawPart(gl, roof4Buffer, roof4Vertices, roof4Color);
-
-  drawPart(gl, doorBuffer, doorVertices, doorColor);
-  drawPart(gl, leftWindowBuffer, leftWindowVertices, leftWindowColor);
-  drawPart(gl, rightWindowBuffer, rightWindowVertices, rightWindowColor);
-
-  requestAnimationFrame(drawScene);
-
-
-}
+    requestAnimationFrame(drawScene);
+  }
 
 
 
